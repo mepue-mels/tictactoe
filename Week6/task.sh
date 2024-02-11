dfe671106def03f3b3b2aaf4c895e371f8655491
MJ Ronduen <rjhronduen@mymail.mapua.edu.ph>
2024-02-11 18:53:59 +0800

    created readline

--- Week6/task.sh
+++ Week6/task.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
 
-time=$(date +"%I:%M %p")
-echo $time
+FILE_NAME='data.txt'
+CURRENT_TIME=$( date | awk '{print $4}' )
+
+if [ -f "$FILE_NAME" ]; then
+    while IFS= read -r line; do
+        echo "Task: $line"
+        if IFS= read -r next_line; then
+            echo "Time: $next_line"
+        fi
+    done < "$FILE_NAME"
+else
+    echo "FILE NOT FOUND"
+fi
